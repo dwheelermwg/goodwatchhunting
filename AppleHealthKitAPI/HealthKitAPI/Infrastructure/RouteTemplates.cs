297c861d37dfc178cc4e3af0eb41d2f0a50f2f45
Adam LaBranche <alabranche@mywebgrocer.com>
2015-12-09 16:44:19 -0500

    Added get all users endpoint.

--- AppleHealthKitAPI/HealthKitAPI/Infrastructure/RouteTemplates.cs
+++ AppleHealthKitAPI/HealthKitAPI/Infrastructure/RouteTemplates.cs
@@ -3,5 +3,6 @@
   public static class RoutesTemplates
   {
     public static readonly string AllEvents = "events/collection";
+    public static readonly string AllUsers = "users/collection";
   }
 }
