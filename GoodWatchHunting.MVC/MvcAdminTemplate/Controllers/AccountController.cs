b12255c4a5e17c2d60a2c4cff5ff3f11810e447a
ckrobotham-mwg <ckrobotham+github@mywebgrocer.com>
2015-12-09 15:01:49 -0500

    Got the MVC project working again

--- GoodWatchHunting.MVC/MvcAdminTemplate/Controllers/AccountController.cs
+++ GoodWatchHunting.MVC/MvcAdminTemplate/Controllers/AccountController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Data.Entity.Validation;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
