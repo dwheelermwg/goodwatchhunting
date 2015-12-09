0128cc9b15906b145632ef49860cd76651b67554
ckrobotham-mwg <ckrobotham+github@mywebgrocer.com>
2015-12-09 14:06:48 -0500

    Added basic controller for Users

---
+++ GoodWatchHunting.MVC/GoodWatchHunting.Services/WebService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodWatchHunting.Services
+{
+  public class WebService
+  {
+    public static T CreateRequest<T>(string apiUrl)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
